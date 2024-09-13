dbb4469cd3d074cdba5f10ed7916a33f59ad594a
Cristian Caroli <cristiancaroli@gmail.com>
2024-09-13 13:47:50 +0200

    Chip exit logic fix

--- Assets/Scripts/CellController.cs
+++ Assets/Scripts/CellController.cs
@@ -6,23 +6,33 @@ public class CellController : MonoBehaviour
 {
     public int row;
     public int column;
-    // Start is called before the first frame update
+    private Stack<char> piecesInside = new Stack<char>();
+    private GameObject GetParentObject(GameObject other) =>
+        other.transform.parent ? other.transform.parent.gameObject : other;
+
     private void OnTriggerEnter(Collider other)
     {
-        GameObject parentObject = other.gameObject.transform.parent ? other.gameObject.transform.parent.gameObject : other.gameObject;
+        GameObject parentObject = GetParentObject(other.gameObject);
         if (parentObject.CompareTag("x_piece"))
         {
+            piecesInside.Push('x');
             BoardController.game.SetPosition(row, column, 'x');
         }
         else if (parentObject.CompareTag("o_piece"))
         {
+            piecesInside.Push('o');
             BoardController.game.SetPosition(row, column, 'o');
         }
     }
 
-    // Called when another object exits the trigger
     private void OnTriggerExit(Collider other)
     {
-        BoardController.game.SetPosition(row, column, ' ');
+        GameObject parentObject = GetParentObject(other.gameObject);
+        if (parentObject.CompareTag("x_piece") || parentObject.CompareTag("o_piece"))
+        {
+            char lastPiece = piecesInside.Pop();
+            BoardController.game.SetPosition(row, column, piecesInside.Count == 0 ? ' ' : piecesInside.Peek()); 
+        }
     }
+
 }
